05a0068b92d5829d0024be5cb1e6bdda4cf32f93
André gois <andre.s.gois3@gmail.com>
2022-01-21 00:20:53 -0300

    making stripe API call

--- README.md
+++ README.md
@@ -32,3 +32,28 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+
+## About application
+
+### Três formas de pegar informações
+- Client-side
+ - quando não precisa de indexação
+ - informação carregada através de uma ação do usuário
+ - EXEMPLO
+   - Post do blog
+   - Conteúdo (SSG)
+   - Comentários (Client-side)
+- Server-side
+ - quando precisa de indexação
+ - dados dinâmicos da sessção do usuário
+- Static Side Generation
+ - Gerar uma página e compartilhar com todos, página de produto, blog, etc... 
+ - páginas que precisam de SEO
+
+## Comments
+
+##### Lucas Cafieiro Alves Bittencourt
+- Aproveitando para agregar um pouco mais de informação, em relação ao seu comentário sobre fazer deploy em Vercel. Muitas vezes quando as pessoas falam sobre o Next.js, amarram muito na questão do deploy com Vercel, mas é possível fazer containerização e deploy de Next.js em qualquer plataforma que tenha suporte a rodar um servidor Node. No meu caso, eu trabalho com GCP, e só precisa de algumas configurações bem simples para colocar o app para rodar no App Engine ou configurá-lo em um container no Cloud Run.
+
+- Como a dúvida já foi muito bem respondida, espero que essa pequena informação possa agregar algo para quem ainda está aprendendo sobre o mundo do deploy e do DevOps.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,36 @@
+import { GetServerSideProps, GetStaticProps } from 'next'
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 import styles from './home.module.scss';
 
-export default function Home() {
+
+/**
+ * Três formas de pegar informações
+ * - Client-side
+ *  - quando não precisa de indexação
+ *  - informação carregada através de uma ação do usuário
+ *  - EXEMPLO
+ *    - Post do blog
+ *    - Conteúdo (SSG)
+ *    - Comentários (Client-side)
+ * - Server-side
+ *  - quando precisa de indexação
+ *  - dados dinâmicos da sessção do usuário
+ * - Static Side Generation
+ *  - Gerar uma página e compartilhar com todos, página de produto, blog, etc... 
+ *  - páginas que precisam de SEO
+ */
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
+  console.log(product);
+
   return (
     <div className="">
       <Head>
@@ -15,14 +43,37 @@ export default function Home() {
           <h1>News about the <span>React</span> World</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt='girl coding' />
       </main>
     </div>
   )
+}
+
+
+
+//export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KKCDCGERzX8Y3EofmCyJCw0', {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24,
+  }
 }
