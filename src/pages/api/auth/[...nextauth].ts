1cf204657d87885ebe5b69d468cdfacd617e6897
André gois <andre.s.gois3@gmail.com>
2022-01-23 18:29:21 -0300

    saving data of the database faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/faunadb";
+import { query as q } from "faunadb";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,4 +17,40 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user;
+      console.log('user.email: '+email)
+      
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get( // como se fosse o select
+              q.Match( // como se fosse o where
+                q.Index('user_by_email'),
+                  q.Casefold(user.email)
+              )
+            )
+          )  
+        )
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+
+    },
+  }
 })
